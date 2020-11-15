6c80b43f0d466e478cca9a1f1190fb70c784672f
kevin <onedayatatime0923@gmail.com>
2020-11-15 16:21:49 +0800

    kevin

--- zsh_setting.sh
+++ zsh_setting.sh
@@ -1,2 +1,3 @@
-sudo apt-get install zsh curl
+sudo apt-get install zsh curl git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
