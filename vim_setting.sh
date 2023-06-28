208b7f39f75d8512395b9401123dfa021977e623
kevin <onedayatatime0923@gmail.com>
2023-06-28 09:51:04 -0700

    kevin

--- vim_setting.sh
+++ vim_setting.sh
@@ -6,7 +6,6 @@ mkdir -p ~/.vim/undodir
 # install plug.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # install youcompleteme
 sudo apt-get install build-essential cmake
 sudo apt-get install python3-dev
